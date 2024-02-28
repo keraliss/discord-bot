32d358835f360408fddef624cd5b860475dfe74e
Basanta Goswami <36882714+basantagoswami@users.noreply.github.com>
2024-02-28 13:50:36 +0530

    remove healthcheck route

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,9 +2,6 @@ import express from "express";
 import registerRoutes from "./register";
 const router = express.Router();
 
-router.get("/healthcheck", async (req, res) => {
-    res.send("OK");
-});
 router.use("/register", registerRoutes);
 
 export default router;
