8830e367e0f318a5c15f329a35f0643372c770fa
Shourya Sharma <sshourya17@gmail.com>
2024-03-20 02:23:33 -0400

    Changed youtube template

--- src/events/ready/check-youtube.ts
+++ src/events/ready/check-youtube.ts
@@ -63,7 +63,7 @@ export default function (client: Client) {
                                     ?.replace("{VIDEO_TITLE}", latestVideo.title)
                                     ?.replace("{CHANNEL_URL}", feed.link || "")
                                     ?.replace("{CHANNEL_NAME}", feed.title || "") ||
-                                `@here,\nNew upload by ${feed.title}.\n${latestVideo.title} | ${latestVideo.link} \nFind more at <${feed.link}>`;
+                                `@here,New upload by ${feed.title}.\n[${latestVideo.title}](${latestVideo.link}) \nFind more at our [channel](<${feed.link}>)`;
                             targetChannel.send(targetMessage);
                         })
                         .catch(() => {});
