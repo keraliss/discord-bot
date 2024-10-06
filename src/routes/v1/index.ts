8988b98adaafd5727b63171efd0d48a26f4568fc
bit-aloo <sshourya17@gmail.com>
2024-10-06 17:43:12 +0530

    add designer form

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import registerRoutes from "./register";
 import devFormRoutes from "./devForm";
 import eduFormRoutes from "./eduForm";
+import designFormRoutes from "./designForm";
 const router = express.Router();
 
 router.use("/register", registerRoutes);
 router.use("/devform", devFormRoutes);
-router.use("/eduform",eduFormRoutes);
+router.use("/eduform", eduFormRoutes);
+router.use("/designform", designFormRoutes);
 
 export default router;
